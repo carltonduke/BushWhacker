0704b5f8e856b814a72ad39f2cf058c167cc31b2
Carlton Duke <43712879+carltonduke@users.noreply.github.com>
2020-10-25 11:08:58 -0500

    Add files via upload

---
+++ BushModelLoader.java
@@ -0,0 +1,18 @@
+package axonos.bushwhacker.util;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.obj.OBJLoader;
+import net.minecraftforge.client.model.obj.OBJModel;
+
+public class BushModelLoader {
+
+    private static final ResourceLocation BUSH_TEXTURE = "";
+
+    public BushModelLoader() {
+
+
+
+
+    }
+
+}
--- RegistryHandler.java
+++ RegistryHandler.java
@@ -1,5 +1,7 @@
-package axonos.bushwhacker;
+package axonos.bushwhacker.util;
 
+import axonos.bushwhacker.BushWhackerMod;
+import axonos.bushwhacker.tools.BushWhackerIT;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.SwordItem;
@@ -8,19 +10,21 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+
+@SuppressWarnings("MagicConstant")
 public class RegistryHandler {
-        // create DeferredRegister object
-        public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BushWhackerMod.MODID);
+    // create DeferredRegister object
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BushWhackerMod.MODID);
 
-        public static void init() {
-            // attach DeferredRegister to the event bus
-            ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
-        }
+    public static void init() {
+        // attach DeferredRegister to the event bus
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
 
-        //Create and Register SwordItem
-        public static final RegistryObject<Item> BUSHWHACKER = ITEMS.register("bushwhacker", () ->
-                new SwordItem(
-                        new BushWhackerIT(), 0, 1, new Item.Properties().group(ItemGroup.COMBAT)
-                )
-        );
+    //Create and Register SwordItem
+    public static final RegistryObject<SwordItem> BUSHWHACKER = ITEMS.register("bushwhacker", () ->
+            new SwordItem(
+                     BushWhackerIT.BUSH, 0, 0.0F, new Item.Properties().group(ItemGroup.COMBAT)
+            )
+    );
 }
