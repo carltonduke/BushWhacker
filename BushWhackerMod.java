bfdfc0f05fd43f2c576cdcd13518f74b10b7abe8
Carlton Duke <43712879+carltonduke@users.noreply.github.com>
2020-10-25 11:08:43 -0500

    Add files via upload

--- BushWhackerMod.java
+++ BushWhackerMod.java
@@ -1,33 +1,21 @@
 package axonos.bushwhacker;
 
-import net.minecraft.item.Item;
-import net.minecraftforge.common.MinecraftForge;
+import axonos.bushwhacker.util.RegistryHandler;
+import net.minecraftforge.client.model.obj.OBJLoader;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraft.item.SwordItem;
+import net.minecraftforge.common.MinecraftForge;
 
 @Mod ("bushwhacker")
 public class BushWhackerMod {
 
     public static final String MODID = "bushwhacker";
-    public static final String NAME = "The BushWhacker";
-    public static final String VERSION = "1.0";
-
-    Item.Properties properties = new Item.Properties();
-    BushWhackerIT item_tier = new BushWhackerIT();
-
-    public int p_2 = 0;
-    public float p_3 = 1;
-
 
     public BushWhackerMod() {
-
-        SwordItem c_sword = null;
-
+        //What does this do? I dunno
         MinecraftForge.EVENT_BUS.register(this);
 
+        //Call method from RegistryHandler to register the sword
         RegistryHandler.init();
     }
-
-
 
 }
